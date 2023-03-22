a610f40b1c5dfe5479f6335dce2d01010cedda95
Henrique Bocca Corrêa <correa.henriqueb@gmail.com>
2023-03-22 07:38:57 -0300

    calculadora

---
+++ calculadora/assets/js/script.js
@@ -0,0 +1,30 @@
+function clean() {
+    document.getElementById('resultado').innerHTML = "";
+}
+
+function insert(num) {
+    var numero = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = numero + num;
+}
+
+function calcular() {
+    var resultado = document.getElementById('resultado').innerHTML;
+    if (resultado) {
+        document.getElementById('resultado').innerHTML = eval(resultado);
+    }
+    else {
+        document.getElementById('resultado').innerHTML = "Nada..."
+    }
+}
+
+function back() {
+    var resultado = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length - 1);
+}
+
+const checkbox =  document.getElementById('checkbox') 
+
+checkbox.addEventListener('change', () => {
+	
+        document.documentElement.style.setProperty(':root', ':root.dark'); 
+});
